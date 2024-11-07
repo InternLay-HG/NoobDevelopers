36250f9027718321bed4fe7c05962ec526e024de
subhadipnoob <subhadip.2023ug4021@iiitranchi.ac.in>
2024-11-07 20:21:59 +0530

    Add files via upload

--- d1.js
+++ d1.js
@@ -15,3 +15,48 @@
         }, 2000);  // Initial delay of 3 seconds
     });
 
+    
+window.addEventListener('scroll', reveal);
+function reveal(){
+var reveals = document.querySelectorAll('.reveal');
+for (var i = 0; i < reveals.length; i++){
+var windowheight = window.innerHeight;
+var revealtop = reveals[i].getBoundingClientRect().top; var revealpoint = 8;
+if (revealtop < windowheight - revealpoint){ reveals[i].classList.add('active');
+}
+else{reveals[i].classList.remove('active');
+}
+}
+}
+
+window.addEventListener('scroll', reveal1);
+function reveal1(){
+var reveals = document.querySelectorAll('.reveal1');
+for (var i = 0; i < reveals.length; i++){
+var windowheight = window.innerHeight;
+var revealtop = reveals[i].getBoundingClientRect().top; var revealpoint = 8;
+if (revealtop < windowheight - revealpoint){ reveals[i].classList.add('active1');
+}
+else{reveals[i].classList.remove('active1');
+}
+}
+}
+
+window.addEventListener('scroll', reveal2);
+function reveal2(){
+var reveals = document.querySelectorAll('.reveal2');
+for (var i = 0; i < reveals.length; i++){
+var windowheight = window.innerHeight;
+var revealtop = reveals[i].getBoundingClientRect().top; var revealpoint = 8;
+if (revealtop < windowheight - revealpoint){ reveals[i].classList.add('active2');
+}
+else{reveals[i].classList.remove('active2');
+}
+}
+}
+
+
+
+
+   
+        
